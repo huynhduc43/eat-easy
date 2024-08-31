c15f745938050e6c961e5f386fd39668e2eea640
huynhduc43 <thanhduc4320@gmail.com>
2024-08-31 14:22:40 +0700

    fix: handle responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 import SplashScreen from "@/app/ui/splash-screen";
 import { useState, useEffect } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Mulish({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
--- app/ui/splash-screen.tsx
+++ app/ui/splash-screen.tsx
@@ -3,37 +3,67 @@ import Image from "next/image";
 export default function SlashScreen() {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <div className="border rounded-full absolute border-[#FFDFCF] border-opacity-40 -left-44 -top-3">
-        <div className="border rounded-full m-6 border-[#FFDFCF] border-opacity-60">
-          <div className="border rounded-full m-6 border-[#FFDFCF] border-opacity-80">
-            <div className="border rounded-full m-6 p-4 border-[#FFDFCF]">
-              <Image
-                src="/foods/food-splash-1-mobile.png"
-                width={200}
-                height={200}
-                className="block md:hidden"
-                alt="Food splash 1"
-              />
+      <div className="border md:border-2 rounded-full border-[#FFDFCF] border-opacity-20 absolute -left-48 -top-10 md:-left-[448px] md:-top-40">
+        <div className="md:border-2 rounded-full m-0 md:m-12 border-[#FFDFCF] border-opacity-0 md:border-opacity-20">
+          <div className="border md:border-2 rounded-full m-6 md:m-12 border-[#FFDFCF] border-opacity-40">
+            <div className="border md:border-2 rounded-full m-6 md:m-12 border-[#FFDFCF] border-opacity-60">
+              <div className="border md:border-2 rounded-full m-6 md:m-12 border-[#FFDFCF] border-opacity-80">
+                <div className="border md:border-2 rounded-full m-6 md:m-12 p-4 md:p-9 border-[#FFDFCF]">
+                  <Image
+                    src="/foods/food-splash-1-mobile.png"
+                    width={200}
+                    height={200}
+                    className="block md:hidden"
+                    alt="Food splash 1"
+                  />
+                  <Image
+                    src="/foods/food-splash-1-desktop.png"
+                    width={390}
+                    height={390}
+                    className="hidden md:block"
+                    alt="Food splash 1"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div className="">
-        <div className="absolute bottom-11 left-10 flex-col text-6xl  ">
-          <div className="text-[#4A4A6A]">Eat</div>
-          <div className="text-[#FF7B2C]">Easy</div>
+        <div className="absolute bottom-11 left-10 ">
+          <div className="flex flex-col text-6xl md:flex-row">
+            <div className="text-my-neutral-700 font-medium">Eat</div>
+            <div className="text-[#FF7B2C] font-bold">Easy</div>
+          </div>
+          <div className="text-[18px] w-[560px] leading-8 pt-6 hidden md:block relative z-10 text-my-neutral-600">
+            Are you tired of scrolling through menus and struggling to decide
+            what to order? Our new restaurant app has got you covered with
+            personalized recommendations from our digital assistant.
+          </div>
         </div>
-        <div className="border rounded-full border-[#FFDFCF] border-opacity-40 absolute -bottom-12 -right-44">
-          <div className="border rounded-full m-6 border-[#FFDFCF] border-opacity-60">
-            <div className="border rounded-full m-6 border-[#FFDFCF] border-opacity-80">
-              <div className="border rounded-full m-6 p-4 border-[#FFDFCF]">
-                <Image
-                  src="/foods/food-splash-2-mobile.png"
-                  width={200}
-                  height={200}
-                  className="block md:hidden "
-                  alt="Food splash 2"
-                />
+
+        <div className="border md:border-2 rounded-full border-[#FFDFCF] border-opacity-20 absolute -bottom-16 -right-52 md:-bottom-52 md:-right-[444px]">
+          <div className="md:border-2 rounded-full m-0 md:m-12 border-[#FFDFCF] border-opacity-0 md:border-opacity-20">
+            <div className="border md:border-2 rounded-full m-6 md:m-12 border-[#FFDFCF] border-opacity-40">
+              <div className="border md:border-2 rounded-full m-6 md:m-12 border-[#FFDFCF] border-opacity-60">
+                <div className="border md:border-2 rounded-full m-6 md:m-12 border-[#FFDFCF] border-opacity-80">
+                  <div className="border md:border-2 rounded-full m-6 md:m-12 p-4 md:p-9 border-[#FFDFCF]">
+                    <Image
+                      src="/foods/food-splash-2-mobile.png"
+                      width={200}
+                      height={200}
+                      className="block md:hidden"
+                      alt="Food splash 2"
+                    />
+                    <Image
+                      src="/foods/food-splash-2-desktop.png"
+                      width={390}
+                      height={390}
+                      className="hidden md:block"
+                      alt="Food splash 2"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
