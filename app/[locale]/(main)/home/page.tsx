d069e5cdf3cf8cf8abc5adf7c4c676120259c2ca
huynhduc43 <thanhduc4320@gmail.com>
2024-10-24 23:51:18 +0700

    feat: fetch random meals

--- app/[locale]/(main)/home/page.tsx
+++ app/[locale]/(main)/home/page.tsx
@@ -1,13 +1,30 @@
-import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
 import { ContentLayout } from '@/app/components';
+import { apiConfig } from '@/config';
+import { TMeal } from '@/app/[locale]/(main)/recipes/types';
 
-export default function Home() {
-  const t = useTranslations('HomePage');
+async function getRandomMeals(): Promise<{ meals: TMeal[] }> {
+  const res = await fetch(
+    `${apiConfig.baseUrl}/${apiConfig.version}/${apiConfig.apiKey}/random.php`,
+    { next: { revalidate: 60 } }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch random meal');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getRandomMeals();
+  const t = await getTranslations('HomePage');
 
   return (
-    <ContentLayout title={t('title')}>
-      <div>{t('title')}</div>
+    <ContentLayout title={t('title')} className="mx-6 sm:mx-[42px]">
+      <div>{t('Todays_Meal')}</div>
+      <div>{JSON.stringify(data.meals[0])}</div>
     </ContentLayout>
   );
 }
